78c88588bd4640db2de53e04fbff48a834688f9a
Yijie Deng <yijied@uw.edu>
2019-05-15 15:09:53 -0700

    initial setup. note that lf characters transformed to crlf by git

---
+++ project.R
@@ -0,0 +1,20 @@
+download_and_load_packages <- function(required_packages) {
+    repo <- "http://cran.us.r-project.org"
+    for (package in required_packages) {
+        if(!eval(parse(text = paste0("require(", package, ")")))) {
+            install.packages(package, repos = repo)
+        }
+        eval(parse(text = paste0("library(", package, ")")))
+    }
+    
+}
+
+## <!-- add needed packages to this vector --> 
+required_packages <- c("data.table", "dplyr", "ggplot2")
+download_and_load_packages(required_packages)
+
+## fread for faster performance.
+sales <- fread("data/Sale_Prices_Msa.csv", stringsAsFactors = F)
+zri_forecasts <- fread("data/ZriForecast_Public.csv", stringsAsFactors = F)
+zhvis <- fread("data/Metro_Zhvi_Summary_AllHomes.csv", stringsAsFactors = F)
+zris <- fread("data/Metro_Zri_AllHomesPlusMultifamily_Summary.csv", stringsAsFactors = F)
